b971ef756dfac7ecad176648b225d2ca34988bc2
lshapz <laurashapiro01@gmail.com>
2017-04-29 13:51:06 -0400

    Done.

---
+++ js/app/services/DomainService.js
@@ -0,0 +1,13 @@
+function DomainService($http){
+  this.getData = function(domain){
+    return $http.get('/rest/domain/' + domain)
+
+  }
+
+
+}
+
+
+angular
+  .module('app')
+  .service('DomainService', DomainService);
---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,13 @@
+function SubRedditService($http){
+  this.getData = function(subreddit){
+    return $http.get('/rest/subreddit/' + subreddit)
+
+  }
+
+
+}
+
+
+angular
+  .module('app')
+  .service('SubRedditService', SubRedditService);
